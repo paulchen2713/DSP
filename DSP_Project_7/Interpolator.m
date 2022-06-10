4345cf5daf06856e0db9c533e8f343feb40e30b3
Shao-Heng <95068443+paulchen2713@users.noreply.github.com>
2022-06-10 12:24:01 +0800

    Add files via upload

---
+++ DSP_Project_7/Interpolator.m
@@ -0,0 +1,174 @@
+%
+% Experiment_For_Sampling_Rate_Increase
+%
+% Interpolator
+%
+clear all;   % clear workspace
+clc;         % clear command window
+N = 33;
+wp = 0.30*pi;
+ws = 0.50*pi;
+% Wp = Ws = 1;
+%
+NH = (N-1)/2;
+%
+%
+P = zeros(NH+1,1);
+Qp = zeros(NH+1,NH+1);
+Qs = zeros(NH+1,NH+1);
+for i=0:NH % 0:1:NH
+    if i==0
+        P(i+1) = -2*wp;
+    else
+        P(i+1) = -2*sin(i*wp)/i;
+    end
+    for j=0:NH
+       if i==0 && j==0
+           Qp(i+1,j+1) = wp;
+           Qs(i+1,j+1) = pi - ws;
+       elseif i==j
+           Qp(i+1,j+1) = 0.5*wp + 0.5*sin((i+j)*wp)/(i+j);
+           Qs(i+1,j+1) = 0.5*(pi - ws) - 0.5*sin((i+j)*ws)/(i+j);
+       else
+           Qp(i+1,j+1) = 0.5*sin((i-j)*wp)/(i-j) + 0.5*sin((i+j)*wp)/(i+j);
+           Qs(i+1,j+1) = -0.5*sin((i-j)*ws)/(i-j) - 0.5*sin((i+j)*ws)/(i+j);
+       end
+    end
+end
+Q = Qp + Qs;
+A = -0.5*inv(Q)*P;
+%
+%
+h = zeros(N,1);
+h(NH+1) = A(1);
+h(1:NH) = 0.5*A(NH+1:-1:2);
+h(NH+2:N) = 0.5*A(2:NH+1);
+%
+% need to show several plots simulteneously
+subplot(2,4,1);
+stem(0:N-1,h);
+xlabel('n');
+ylabel('Impulse Response');
+title('Case 1 Lowpass Filter');
+%
+% Amplitude Response
+subplot(2,4,5);
+AR = abs(freqz(h,1,0:pi/200:pi));
+plot(0:1/200:1,AR);
+axis([0,1,0,1.1]);
+xlabel('Normalized Frequency (\omega/\pi)');
+ylabel('Amplitude Response');
+title('Case 1 Lowpass Filter');
+%
+% Amplitude Response in dB
+% subplot(2,2,4);
+% plot(0:1/200:1,20*log10(AR));
+% axis([0,1,-60,10]);
+% xlabel('Normalized Frequency (\omega/\pi)');
+% ylabel('Amplitude Response in dB');
+%
+% L = 3
+%
+h3 = zeros(3*(N-1) + 1, 1);
+for i = 0:3*(N-1)
+    if mod(i, 3) == 0
+        h3(i + 1) = h(i/3 + 1);
+    end
+end
+subplot(2, 4, 2);
+stem(0:3*(N-1),h3);
+xlabel('n');
+title('Up Sampling L = 3');
+%
+% 
+subplot(2, 4, 6);
+FR3 = abs(freqz(h3,1,0:pi/200:pi));
+plot(0:1/200:1,FR3);
+axis([0,1,0,1.1]);
+xlabel('Normalized Frequency (\omega/\pi)');
+ylabel('Amplitude Response');
+title('Up Sampling L = 3');
+%
+% design of interpolation filter
+%
+N = 33;
+wp = 0.5*pi/3;
+ws = 1.5*pi/3;
+% Wp = Ws = 1;
+%
+NH = (N-1)/2;
+%
+%
+P = zeros(NH+1,1);
+Qp = zeros(NH+1,NH+1);
+Qs = zeros(NH+1,NH+1);
+for i=0:NH % 0:1:NH
+    if i==0
+        P(i+1) = -2*wp;
+    else
+        P(i+1) = -2*sin(i*wp)/i;
+    end
+    for j=0:NH
+       if i==0 && j==0
+           Qp(i+1,j+1) = wp;
+           Qs(i+1,j+1) = pi - ws;
+       elseif i==j
+           Qp(i+1,j+1) = 0.5*wp + 0.5*sin((i+j)*wp)/(i+j);
+           Qs(i+1,j+1) = 0.5*(pi - ws) - 0.5*sin((i+j)*ws)/(i+j);
+       else
+           Qp(i+1,j+1) = 0.5*sin((i-j)*wp)/(i-j) + 0.5*sin((i+j)*wp)/(i+j);
+           Qs(i+1,j+1) = -0.5*sin((i-j)*ws)/(i-j) - 0.5*sin((i+j)*ws)/(i+j);
+       end
+    end
+end
+Q = Qp + Qs;
+A = -0.5*inv(Q)*P;
+%
+%
+hL = zeros(N,1);
+hL(NH+1) = A(1);
+hL(1:NH) = 0.5*A(NH+1:-1:2);
+hL(NH+2:N) = 0.5*A(2:NH+1);
+%
+% need to show several plots simulteneously
+subplot(2,4,3);
+stem(0:N-1,hL);
+xlabel('n');
+ylabel('Impulse Response');
+% title('Case 1 Lowpass Filter');
+%
+% Amplitude Response
+subplot(2,4,7);
+FRL = abs(freqz(hL,1,0:pi/200:pi));
+plot(0:1/200:1,FRL);
+axis([0,1,0,1.1]);
+xlabel('Normalized Frequency (\omega/\pi)');
+ylabel('Amplitude Response');
+% title('Case 1 Lowpass Filter');
+%
+%
+hI = conv(h3, hL);
+subplot(2, 4, 4);
+stem(0:128,hI);
+xlabel('n');
+% title('Up Sampling L = 3');
+%
+% 
+subplot(2, 4, 8);
+FRI = abs(freqz(hI,1,0:pi/200:pi));
+plot(0:1/200:1,FRI);
+axis([0,1,0,1.1]);
+xlabel('Normalized Frequency (\omega/\pi)');
+ylabel('Amplitude Response');
+pause;
+% title('Up Sampling L = 3');
+%
+%
+subplot(3, 1, 1);
+stem(h);
+subplot(3, 1, 2);
+stem(h3);
+subplot(3, 1, 3);
+stem(hI);
+%
+%
