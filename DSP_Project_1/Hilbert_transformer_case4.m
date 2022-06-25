c167ea83811f8e7d9c8caa17dc97f1ba7a3d58dd
Shao-Heng <95068443+paulchen2713@users.noreply.github.com>
2022-06-25 20:49:12 +0800

    Update Hilbert_transformer_case4.m

--- DSP_Project_1/Hilbert_transformer_case4.m
+++ DSP_Project_1/Hilbert_transformer_case4.m
@@ -1,33 +1,32 @@
 %
-% Design of case 4 ¢Öilbert_transformers
+% Design of case 4 Â¢Ã–ilbert_transformers
 % 
-clear all;
+clear;
 clc;
-N=30;
-wp1=0.05*pi;
-%
+% 
+N   = 30;
+wp1 = 0.05*pi;
 %
 NH=N/2;
 %
-%
-P=zeros(NH,1);
-Q=zeros(NH,NH);
-for i=1:NH
-    P(i)=2*cos((i-0.5)*wp1)/(i-0.5);    
-    for j=1:NH
-        if i==j
-            Q(i,j)=0.5*(pi-wp1)+0.5*sin((i+j-1)*wp1)/(i+j-1);            
+P = zeros(NH,1);
+Q = zeros(NH,NH);
+for i = 1:NH
+    P(i) = 2*cos((i-0.5)*wp1)/(i-0.5);    
+    for j = 1:NH
+        if i == j
+            Q(i, j) = 0.5*(pi-wp1)+0.5*sin((i+j-1)*wp1)/(i+j-1);            
         else
-            Q(i,j)=0.5*sin((i+j-1)*wp1)/(i+j-1)-0.5*sin((i-j)*wp1)/(i-j) ;              
+            Q(i, j) = 0.5*sin((i+j-1)*wp1)/(i+j-1)-0.5*sin((i-j)*wp1)/(i-j) ;              
         end
     end   
 end    
-A=-0.5*inv(Q)*P;
+A = -0.5*inv(Q)*P;
 %
 %
-h=zeros(N,1);
-h(1:NH)=0.5*A(NH:-1:1);
-h(NH+1:N)=-0.5*A;
+h = zeros(N,1);
+h(1:NH)   = 0.5*A(NH:-1:1);
+h(NH+1:N) = -0.5*A;
 %
 %
 subplot(1,2,1);
@@ -37,7 +36,7 @@ ylabel('Impulse response');
 %
 %
 subplot(1,2,2);
-AR=abs(freqz(h,1,0:pi/200:pi));
+AR = abs(freqz(h,1,0:pi/200:pi));
 plot(0:1/200:1,AR);
 axis([0,1,0,1.1]);
 xlabel('Normalized frequency (\omega/\pi/)');
